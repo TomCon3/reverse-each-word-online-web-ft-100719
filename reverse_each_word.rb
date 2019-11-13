c8f519a957c526e70444cd0522959a98620d150a
Tommy Connor <tconnor98@aol.com>
2019-11-13 23:05:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,14 +1,1 @@
-
-
-
-
-
-
-def reverse_each_word(sentence1)
-  array = sentence1.split("")
-  new_array = []
-  array.each do |sentence1|
-    new_array.push(sentence1.reverse)
-  end
-  new_array.join("")
-end
+reverse_each_word(sentence1)
