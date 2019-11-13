1ffb77607802404e417ae00a110110c076617016
Tommy Connor <tconnor98@aol.com>
2019-11-13 23:10:05 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,12 +6,3 @@ array.collect do |sentence1|
 end
 test_array.join("")
 end
-
-def reverse_each_word(sentence1)
-  array = sentence1.split("")
-  new_array = []
-  array.each do |sentence1|
-    new_array.push(sentence1.reverse)
-  end
-  new_array.join("")
-end
